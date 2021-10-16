85392920675e9c9cb65cdbc9abbfb9018a866e0a
GreeshmithRacharla <76734059+GreeshmithRacharla@users.noreply.github.com>
2021-10-16 19:03:21 +0530

    Add files via upload

---
+++ Feed.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Feed extends React.Component{
+    render(){
+        return(
+            <View 
+            style={{
+                flex : 1 ,
+                justifyContent : "center",
+                alignItems : "center"
+            }}>
+                <Text>Feed</Text>
+            </View>
+        )
+    }
+}
